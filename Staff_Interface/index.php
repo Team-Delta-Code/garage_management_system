98b48819ee3083522f940ee06e5b419d5c0a98f2
Madushanka Premakumara <madushankawideworld@gmail.com>
2025-02-08 16:22:47 +0530

    Login Bypass

--- Staff_Interface/index.php
+++ Staff_Interface/index.php
@@ -2,13 +2,14 @@
 //start session
 session_start();
 
+//bypass login
 //verify existing session
-if(isset($_SESSION['employee_id'])) {
-	// echo "Welcome, user with ID: " . $_SESSION['employee_id'];
-	header("Location: dashboard.php");
-} else {
-	// echo "Please Login";
-	header("Location: login.php");
-    exit();
-}
+// if(isset($_SESSION['employee_id'])) {
+// 	// echo "Welcome, user with ID: " . $_SESSION['employee_id'];
+ 	header("Location: dashboard.php");
+// } else {
+// 	// echo "Please Login";
+// 	header("Location: login.php");
+//     exit();
+// }
 ?>
--- Staff_Interface/main/sessionChecker.php
+++ Staff_Interface/main/sessionChecker.php
@@ -2,12 +2,14 @@
 // Start session
 session_start();
 
+//bypass login
+
 // Verify existing session using a not clause
-if (!isset($_SESSION['employee_id'])) {
-    // If the session does not exist, redirect to the login page
-    header("Location: login.php");
-    exit();
-}
+// if (!isset($_SESSION['employee_id'])) {
+//     // If the session does not exist, redirect to the login page
+//     header("Location: login.php");
+//     exit();
+// }
 
 //database connection
 include('main/connect.php');
